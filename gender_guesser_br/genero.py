06e3ca96835d7854d95dddf23687f4ea624a8f10
GusFurtado <gustavofurtado2@gmail.com>
2023-10-22 21:33:42 -0300

    📝 Adicionar docstring

--- gender_guesser_br/genero.py
+++ gender_guesser_br/genero.py
@@ -34,6 +34,29 @@ class Genero:
     }
 
     def __init__(self, nome: str, uf: Optional[Union[int, str]] = None):
+        """Identificador de Gênero
+
+        Parameters
+        ----------
+        nome : str
+            Nome próprio que se deseja conhecer o gênero.
+        uf : int | str, optional
+            Sigla ou código IBGE da Unidade Federativa
+
+        Properties
+        ----------
+        f : dict[str, float]
+            Proporções do gênero feminino.
+        m : dict[str, float]
+            Proporções do gênero masculino.
+
+        Methods
+        -------
+        __call__(corte_ambos: float = 0.6, corte_maioria: float = 0.8) -> str
+            Retorna um texto com a previsão de gênero.
+
+        """
+
         # Converte a sigla da UF em código IBGE
         if isinstance(uf, str):
             uf = self._UFS[uf]
@@ -42,6 +65,27 @@ class Genero:
         self._m = self._gender_freq(nome, sexo="M", uf=uf)
 
     def __call__(self, corte_ambos: float = 0.6, corte_maioria: float = 0.8) -> str:
+        """Retorna um texto com a previsão de gênero.
+
+        Parameters
+        ----------
+        corte_ambos : float = 0.6
+            A partir de que proporção o nome é considerado de um gênero só ao invés de ambos.
+        corte_maioria : float = 0.8
+            A partir de que proporção temos certeza de que um nome pertence a tal gênero.
+
+        Returns
+        -------
+        str
+            Uma das seis possíveis previsões, baseado nos valores de corte:
+            - "desconhecido"
+            - "feminino"
+            - "provavelmente_feminino"
+            - "masculino"
+            - "provavelmente_masculino"
+            - "ambos"
+
+        """
         # Validar argumentos
         if corte_ambos < 0.5 or corte_ambos > 1:
             raise ValueError("Insira um valor entre 0.5 e 1.0 para `corte_ambos`.")
@@ -66,15 +110,24 @@ class Genero:
         else:
             return "ambos"
 
-    def _gender_freq(nome: str, sexo: Literal["f", "m"], uf: Optional[int] = None):
-        try:
-            freq = ibge.nomes(nome, sexo=sexo, localidade=uf)
-            return freq.squeeze().sum()
-        except ValueError:
-            return 0
+    @property
+    def f(self):
+        """Proporções do gênero feminino."""
+
+        if not (self._f + self._m):
+            p = 0
+        else:
+            p = self._f / (self._f + self._m)
+
+        return {
+            "absoluto": self._f,
+            "percentual": p,
+        }
 
     @property
     def m(self):
+        """Proporções do gênero masculino."""
+
         if not (self._f + self._m):
             p = 0
         else:
@@ -85,14 +138,9 @@ class Genero:
             "percentual": p,
         }
 
-    @property
-    def f(self):
-        if not (self._f + self._m):
-            p = 0
-        else:
-            p = self._f / (self._f + self._m)
-
-        return {
-            "absoluto": self._f,
-            "percentual": p,
-        }
+    def _gender_freq(nome: str, sexo: Literal["f", "m"], uf: Optional[int] = None):
+        try:
+            freq = ibge.nomes(nome, sexo=sexo, localidade=uf)
+            return freq.squeeze().sum()
+        except ValueError:
+            return 0
