64fc5a6f8e3108e24bd603a006d95faba887d770
GusFurtado <gustavofurtado2@gmail.com>
2024-02-12 22:53:26 -0300

    🔖 1.0.0

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="gender_guesser_br",
+    packages=["gender_guesser_br"],
+    version="1.0.0",
+    license="MIT",
+    python_requires=">=3.6",
+    description="Versão brasileira do pacote Python para adivinhar o gênero de um nome próprio.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Gustavo Furtado",
+    author_email="gustavofurtado2@gmail.com",
+    url="https://github.com/GusFurtado/gender_guesser_br",
+    download_url="https://github.com/GusFurtado/gender_guesser_br/archive/1.0.0.tar.gz",
+    keywords=["brasil", "ibge", "dadosabertos", "gender", "genero"],
+    install_requires=["DadosAbertosBrasil"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Portuguese (Brazilian)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
